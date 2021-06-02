b20a4f41e54114d11b8a48cfdb77c76499d041d6
Alex LaGrassa <lagrassa@cmu.edu>
2021-06-02 15:01:18 -0400

    Seems to work okay, made more configurable

--- perception_utils/box_detector.py
+++ perception_utils/box_detector.py
@@ -15,50 +15,73 @@ class BoxDetector:
         self._saturation_high = cfg["saturation_high"] #400
         self._value_low = cfg["value_low"] #200
         self._value_high = cfg["value_high"] #400
+        self._min_area = cfg["min_area"] # 1000
+
+    def detect_from_thresh(self,  thresh, image,vis=False, verbose=True):
+        contours, hier = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        img =  image
+        detections = []
+        for c in contours:
+            # get the bounding rect
+            x, y, w, h = cv2.boundingRect(c)
+            if w*h < self._min_area:
+                continue
+            print("Area", w*h)
+            # draw a green rectangle to visualize the bounding rect
+            #cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 5)
+            # get the min area rect
+            rect = cv2.minAreaRect(c)
+            print("Center", rect[0])
+            print("Angle", rect[-1])
+            box = cv2.boxPoints(rect)
+            # convert all coordinates floating point values to int
+            box = np.int0(box)
+            # draw a red 'nghien' rectangle
+            cv2.drawContours(img, [box], 0, (0, 0, 255), thickness=20)
+            detections.append((rect[0], rect[-1]))
+
+        print(f"Detected {len(detections)} over the minimum area of {self._min_area}")
+        if vis:
+            plt.imshow(thresh, cmap="gray")
+            plt.show()
+            cv2.drawContours(img, contours, -1, (255, 255, 0), 1)
+            plt.imshow(img)
+            plt.show()
+        return detections
+
 
     def detect_from_frames(self, color_im, depth_im, intr, vis=False):
         camera_params = [getattr(intr, v) for v in ['fx', 'fy', 'cx', 'cy']]
         #convert to HSV here
         gray_frame = cv2.cvtColor(color_im.data, cv2.COLOR_BGR2GRAY)
         hsv = cv2.cvtColor(color_im.data, cv2.COLOR_BGR2HSV)
+        image = color_im.data.copy() #TODO make color?
+
+
         hue_only = hsv[:,:,0]
         saturation_only = hsv[:,:,1]
         value_only = hsv[:,:,2]
-        plt.imshow(hue_only)
-        plt.show()
-        import ipdb; ipdb.set_trace()
+
         hue_mask = np.logical_and(hue_only > self._hue_low, hue_only < self._hue_high)
-        plt.imshow(hue_only * hue_mask, cmap="gray")
-        plt.show()
         saturation_mask = np.logical_and(saturation_only > self._saturation_low, saturation_only < self._saturation_high)
         value_mask = np.logical_and(value_only > self._value_low, value_only < self._value_high)
-        plt.imshow(hue_only * hue_mask * saturation_mask*value_mask, cmap="gray")
-        plt.show()
-
-        if vis:
-            plt.figure()
-            mask_idx = np.argwhere(dimg)
-            overlay = color_im.data.copy()
-            overlay[mask_idx[:,0], mask_idx[:,1]] = np.array([0, 1, 0]) * 255
-            plt.imshow(overlay)
-            plt.show()
-
+        thresh = hue_only * hue_mask * saturation_mask*value_mask
+        vis=0
+        detections = self.detect_from_thresh(thresh, image, vis=vis)
+        import ipdb; ipdb.set_trace()
         T_tag_cameras = []
         for detection in detections:
-            M, _, _ = self._detector.detection_pose(detection, camera_params, tag_size=self._cfg['tag_size'])
-
-            det_px_center = np.round(detection.center).astype('int')
+            center, yaw = detection
+            det_px_center = np.round(center).astype('int')
             det_px_center_pt = Point(det_px_center, frame=intr.frame)
             det_px_depth = depth_im[det_px_center[1], det_px_center[0]]
 
             det_px_center_pt_3d = intr.deproject_pixel(det_px_depth, det_px_center_pt)
+            rotation = RigidTransform.z_axis_rotation(-yaw) #only works with overhead camera really then
 
             T_tag_cameras.append(RigidTransform(
-                rotation=M[:3, :3], translation=det_px_center_pt_3d.data,
-                from_frame='{}/{}'.format(
-                    detection.tag_family.decode(), detection.tag_id
-                ), to_frame=intr.frame
-            ))
+                rotation=rotation, translation=det_px_center_pt_3d.data,
+                from_frame='pencil/0'))
 
         return T_tag_cameras
 
