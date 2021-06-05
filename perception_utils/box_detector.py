bc93c7aff5753f463191d6c5cb25290cd43e70f6
Alex LaGrassa <lagrassa@cmu.edu>
2021-06-05 00:31:37 -0400

    being able to use the blue and pink boxes

--- perception_utils/box_detector.py
+++ perception_utils/box_detector.py
@@ -9,12 +9,10 @@ from autolab_core import Point, RigidTransform
 class BoxDetector:
 
     def __init__(self, cfg):
-        self._hue_low = cfg["hue_low"]
-        self._hue_high = cfg["hue_high"]
-        self._saturation_low = cfg["saturation_low"] #200
-        self._saturation_high = cfg["saturation_high"] #400
-        self._value_low = cfg["value_low"] #200
-        self._value_high = cfg["value_high"] #400
+        self._color_to_bounds = {}
+        for color in cfg["colors"]:
+            self._color_to_bounds[color] = {"low":cfg["colors"][color]["low"], "high":cfg["colors"][color]["high"]}
+        import ipdb; ipdb.set_trace()
         self._min_area = cfg["min_area"] # 1000
 
     def detect_from_thresh(self,  thresh, image,vis=False, verbose=True):
@@ -61,27 +59,29 @@ class BoxDetector:
             plt.show()
         return detections
 
-    def get_thresh(self, color_im, vis=False):
-        gray_frame = cv2.cvtColor(color_im.data, cv2.COLOR_BGR2GRAY)
-        hsv = cv2.cvtColor(color_im.data, cv2.COLOR_BGR2HSV)
-        image = color_im.data.copy() #TODO make color?
-
-
+    def _get_mask(self,hsv, color):
         hue_only = hsv[:,:,0]
         saturation_only = hsv[:,:,1]
         value_only = hsv[:,:,2]
+        #hue_mask = np.logical_and(hue_only > self._color_to, hue_only < self._hue_high)
+        #saturation_mask = np.logical_and(saturation_only > self._saturation_low, saturation_only < self._saturation_high)
+        #value_mask = np.logical_and(value_only > self._value_low, value_only < self._value_high)
+        #thresh = hue_only * hue_mask * saturation_mask*value_mask
+        fits_low = np.sum(hsv < np.array(self._color_to_bounds[color]["low"]),axis=2)==3
+        fits_high = np.sum(hsv < np.array(self._color_to_bounds[color]["high"]),axis=2)==3
+        thresh = np.logical_and(fits_low, fits_high)
 
-        hue_mask = np.logical_and(hue_only > self._hue_low, hue_only < self._hue_high)
-        saturation_mask = np.logical_and(saturation_only > self._saturation_low, saturation_only < self._saturation_high)
-        value_mask = np.logical_and(value_only > self._value_low, value_only < self._value_high)
-        thresh = hue_only * hue_mask * saturation_mask*value_mask
-        return thresh, image
+        import ipdb; ipdb.set_trace()
+        return thresh
 
 
     def detect_from_frames(self, color_im, depth_im, intr, vis=False):
         camera_params = [getattr(intr, v) for v in ['fx', 'fy', 'cx', 'cy']]
         #convert to HSV here
-        thresh, image = self.get_thresh(color_im, vis=vis)
+        gray_frame = cv2.cvtColor(color_im.data, cv2.COLOR_BGR2GRAY)
+        hsv = cv2.cvtColor(color_im.data, cv2.COLOR_BGR2HSV)
+        image = color_im.data.copy() 
+        thresh = self._get_mask(thresh, 'red')
         endpoints = self.detect_from_thresh(thresh, image, vis=vis)
         T_tag_cameras = []
         for i, endpoint in enumerate(endpoints):
@@ -111,7 +111,7 @@ class InHandBoxDetector(BoxDetector):
         super().__init__(cfg)
         self._min_area = 10
 
-    def detect_from_thresh(self,  thresh, image,vis=False, verbose=True):
+    def detect_from_thresh(self,  thresh,  image,vis=False, verbose=True):
         contours, hier = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         img =  image
         detections = []
@@ -151,6 +151,8 @@ class InHandBoxDetector(BoxDetector):
             detections = rect[0], w*h
 
         print(f"Detected {len(detections)} over the minimum area of {self._min_area}")
+        if len(detections) == 0:
+            return None, 0
         import ipdb; ipdb.set_trace()
         if vis:
             plt.imshow(thresh, cmap="gray")
@@ -162,12 +164,19 @@ class InHandBoxDetector(BoxDetector):
 
     def detect_from_frames(self, color_im, depth_im, intr, vis=False):
         camera_params = [getattr(intr, v) for v in ['fx', 'fy', 'cx', 'cy']]
-        #convert to HSV here
-        import ipdb; ipdb.set_trace()
-        thresh, image = self.get_thresh(color_im, vis=vis)
-        center, area = self.detect_from_thresh(thresh, image, vis=vis)
+        gray = cv2.cvtColor(color_im.data, cv2.COLOR_BGR2GRAY)
+        hsv = cv2.cvtColor(color_im.data, cv2.COLOR_BGR2HSV)
+        image = color_im.data.copy() 
+        red_thresh = self._get_mask(hsv, 'red')
+        pink_thresh = self._get_mask(hsv, 'pink')
+        blue_thresh = self._get_mask(hsv, 'blue')
+        red_center, red_area = self.detect_from_thresh(gray*red_thresh, image, vis=vis)
+        if red_center is None:
+            return []
+        pink_center, pink_area = self.detect_from_thresh(gray*pink_thresh, image, vis=vis)
+        blue_center, blue_area = self.detect_from_thresh(gray*blue_thresh, image, vis=vis)
         T_tag_cameras = []
-        det_px_center = np.round(center).astype('int')
+        det_px_center = np.round(red_center).astype('int')
         det_px_center_pt = Point(det_px_center, frame=intr.frame)
         det_px_depth = depth_im[det_px_center[1], det_px_center[0]]
         if det_px_depth > 0.2: #probably on the ground, just visible from the camera
@@ -176,24 +185,28 @@ class InHandBoxDetector(BoxDetector):
             return []
 
         det_px_center_pt_3d = intr.deproject_pixel(det_px_depth, det_px_center_pt)
-        if area < 50000: #this is where it might not be centered
+        if red_area < 50000: #this is where it might not be centered
             #check if it can see the pink, and check if it can see the blue
             #if it can see the pink, call it 8cm. If it can see the blue, call it 10cm. 
             #that's the best we can do with the realsense
-            pass
+            small_min_area = 30
+            if blue_area > small_min_area:
+                x_offset = 0.12 + np.random.uniform(low=0.01, high = 0.01)
+            if pink_area > small_min_area and blue_area < small_min_area:
+                x_offset = 0.08 + np.random.uniform(low=0.01, high = 0.01)
             
         #Very hacky and manual method...
         slope = 0.00012215998849647464 #from other script
         intercept = 0.3821936490386042
-        x_offset = slope*area + intercept
-        translation = np.array([area,0,x_offset])
+        x_offset = slope*red_area + intercept
+        translation = np.array([x_offset,0,0.08])
         
         #rotation = RigidTransform.z_axis_rotation(yaw + np.pi/2) #only works with overhead camera really then
         #c_yaw, s_yaw = np.cos(yaw), np.sin(yaw)
         #rotation_alt = np.array([[c_yaw, s_yaw, 0],[s_yaw, -c_yaw, 0],[0, 0, -1]])
 
         T_tag_cameras.append(RigidTransform(
-            rotation=np.eye(3), translation=translation,
+            rotation=RigidTransform.z_axis_rotation(np.pi/2), translation=translation,
             from_frame=f'pencil_inhand/0'))
 
 
